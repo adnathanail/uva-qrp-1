4d7e34c97393e5e4f79c0024d0590ab25a2171cc
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-02-02 23:09:46 +0100

    Fix confusing comments

--- algorithm-1/lib/measurements.py
+++ algorithm-1/lib/measurements.py
@@ -9,8 +9,8 @@ def measure_bell_basis(qc: QuantumCircuit, qubits_A: list, qubits_B: list, clbit
     (CNOT then H) and measure in the computational basis.
 
     The measurement outcome directly gives us y = (a, b) where:
-    - The B register measurement gives 'a' (which controlled Z)
-    - The A register measurement gives 'b' (which controlled X)
+    - The A register measurement gives 'a' (Z info, via H converting phase to bit)
+    - The B register measurement gives 'b' (X info, via CNOT propagating bit flips)
 
     Args:
         qc: Quantum circuit to modify
@@ -27,8 +27,8 @@ def measure_bell_basis(qc: QuantumCircuit, qubits_A: list, qubits_B: list, clbit
         qc.cx(qubits_A[i], qubits_B[i])
         qc.h(qubits_A[i])
 
-    # Measure: B register gives 'a', A register gives 'b'
-    # Store as y = (a, b) = (B measurement, A measurement)
+    # Measure: A register gives 'a' (Z info), B register gives 'b' (X info)
+    # Store as y = (a, b) = (A measurement, B measurement)
     for i in range(n):
-        qc.measure(qubits_B[i], clbits[i])  # a bits
-        qc.measure(qubits_A[i], clbits[n + i])  # b bits
+        qc.measure(qubits_A[i], clbits[i])      # a bits (Z info)
+        qc.measure(qubits_B[i], clbits[n + i])  # b bits (X info)
