9737e67e9657fd29f8f6610389c43429476e5744
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-02-16 22:16:27 +0100

    Ensure no collisions between UNITARIES dictionaries

--- algorithm-1/lib/unitaries/__init__.py
+++ algorithm-1/lib/unitaries/__init__.py
@@ -3,6 +3,11 @@
 from lib.unitaries.standard import STANDARD_UNITARIES
 from lib.unitaries.stim_random_cliffords import STIM_UNITARIES
 
+# Ensure no collisions between the dictionaries
+_overlap = set(STANDARD_UNITARIES) & set(STIM_UNITARIES)
+if _overlap:
+    raise ValueError(f"Duplicate unitary names: {_overlap}")
+
 UNITARIES = {**STANDARD_UNITARIES, **STIM_UNITARIES}
 
 __all__ = [
