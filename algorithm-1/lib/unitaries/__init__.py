a76153ca10f89562947d69ecafce507a735ebe6f
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-02-16 22:04:38 +0100

    Create freeze new stim Clifford script

--- algorithm-1/lib/unitaries/__init__.py
+++ algorithm-1/lib/unitaries/__init__.py
@@ -1,11 +1,5 @@
 """Registry of unitary gates for the Clifford tester harness."""
 
-from lib.unitaries.generators import (
-    freeze_gate,
-    stim_random_clifford_circuit,
-    stim_random_clifford_gate,
-)
-from lib.unitaries.generators.stim import freeze_stim_clifford
 from lib.unitaries.standard import STANDARD_UNITARIES
 from lib.unitaries.stim_random_cliffords import STIM_UNITARIES
 
@@ -15,8 +9,4 @@ __all__ = [
     "STANDARD_UNITARIES",
     "STIM_UNITARIES",
     "UNITARIES",
-    "freeze_gate",
-    "freeze_stim_clifford",
-    "stim_random_clifford_circuit",
-    "stim_random_clifford_gate",
 ]
---
+++ algorithm-1/scripts/freeze_new_stim_clifford.py
@@ -0,0 +1,3 @@
+from lib.unitaries.generators import freeze_stim_clifford
+
+freeze_stim_clifford(3)
