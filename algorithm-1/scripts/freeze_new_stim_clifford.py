ad7be1658ee22a6e3ab6d8449583078b45995111
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-02-16 22:19:00 +0100

    Make number of qubits configurable in freeze_new_stim_clifford.py

--- algorithm-1/scripts/freeze_new_stim_clifford.py
+++ algorithm-1/scripts/freeze_new_stim_clifford.py
@@ -1,3 +1,11 @@
+# uv run algorithm-1/scripts/freeze_new_stim_clifford.py 4
+import argparse
+
 from lib.unitaries.generators import freeze_stim_clifford
 
-freeze_stim_clifford(3)
+parser = argparse.ArgumentParser(description="Freeze a random Stim Clifford unitary.")
+parser.add_argument("n", type=int, help="Number of qubits")
+args = parser.parse_args()
+
+name = freeze_stim_clifford(args.n)
+print(f"Frozen: {name}")
